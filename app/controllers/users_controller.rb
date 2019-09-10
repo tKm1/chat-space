e172042f42f05358f65e865f545c24e45a414241
tKm1 <tk3rw_falilv@yahoo.co.jp>
2019-09-10 12:27:05 +0900

    add users information edit 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,11 @@ class UsersController < ApplicationController
     if current_user.update(user_params)
       redirect_to root_path
     else
+      render :edit
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
-
