4991119feac5872d523153a9c231d6ca29021237
tKm1 <tk3rw_falilv@yahoo.co.jp>
2019-09-16 17:51:58 +0900

    HTMLをJSに追加

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,22 @@
 $(function() {
+
+  var search_list = $("#user-search-result")
+
+  function appendUserName(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
+    search_list.append(html);
+  }
+
+  function appendNoUserName(msg) {
+    var html = `<p>
+                  <div class="chat-group-user__name'>${fail_comment}</div>
+                </p>`
+    search_list.append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
 
@@ -12,7 +30,7 @@ $(function() {
     .done(function(users) {
       if (users.length !== 0) {
         users.forEach(function(user){
-          appendName(user);
+          appendUserName(user);
         });
       }
       else {
