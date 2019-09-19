0ab3c61f5d67059c25a4917e764399e74696ed43
tKm1 <tk3rw_falilv@yahoo.co.jp>
2019-09-19 19:15:59 +0900

    エラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,10 @@ class UsersController < ApplicationController
       format.json
     end
   end
-
+  
   def edit
   end
-
+  
   def update
     if current_user.update(user_params)
       redirect_to root_path
