0fbfd979c625707cba31649d1eec9dbf06e3ce14
tKm1 <tk3rw_falilv@yahoo.co.jp>
2019-09-22 19:52:26 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.1"
+lock "3.11.1"
 
 set :application, "chat-space"
 set :repo_url, "git@github.com:tKm1/chat-space.git"
